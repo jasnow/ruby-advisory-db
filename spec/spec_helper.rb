83a77f10e125c5f8575fd79ca0e8df95fbdc2d35
Al Snow <jasnow@hotmail.com>
2023-06-30 18:54:45 -0400

    WIP: Broken branch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,14 @@ require 'kwalify'
 require 'rspec'
 
 ROOT = File.expand_path(File.join(__dir__,'..'))
+
+RSpec::Matchers.define :match_array_with_extra_elements do |expected|
+  match do |actual|
+    extra_elements = expected.each { |e| actual.delete(e) }
+    if extra_elements.empty?
+      true
+    else
+      RSpec::Expectations.fail_with("Extra elements found: #{extra_elements}")
+    end
+  end
+end
